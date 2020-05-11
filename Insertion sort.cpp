d21ca816ceea2dbb1a22bf2a1885f3ed437c1606
ericktq <64106507+ericktq@users.noreply.github.com>
2020-05-11 15:54:42 -0500

    Add files via upload

---
+++ Insertion sort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int numeros[] = {3,4,2,1,5};
+	int i,pos,aux;
+	
+	for(i=0;i<5;i++){
+		pos = i; 
+		aux = numeros[i];
+		
+		while((pos>0) && (numeros[pos-1] > aux)){
+			numeros[pos] = numeros[pos-1];
+			pos--;
+		}
+		numeros[pos] = aux;
+	}
+	
+	for(i=0;i<5;i++){
+		cout<<numeros[i]<<" ";
+	}
+	return 0;
+	}
+	
+
+
---
+++ selection sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int numeros[] = {3,4,5,2,1};
+	int i,j,aux,min;
+	
+	//Algoritmo del Ordenamiento por Selección
+	for(i=0;i<5;i++){
+		min = i;
+		for(j=i+1;j<5;j++){
+			if(numeros[j] < numeros[min]){
+				min = j;
+			}
+		}
+		aux = numeros[i];
+		numeros[i] = numeros[min];
+		numeros[min] = aux;
+	}
+	
+	
+	cout<<"Orden Ascendente: ";
+	for(i=0;i<5;i++){
+		cout<<numeros[i]<<" ";
+	}
+	
+
+	return 0;
+}
