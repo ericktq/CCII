5209ab75f4e2d5cbb1a6e925cfc1978d37cbf947
ericktq <64106507+ericktq@users.noreply.github.com>
2020-05-12 14:27:27 -0500

    Add files via upload

---
+++ QUICKSORT0.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;  
+  
+
+void cambio(int* a, int* b)  
+{  
+    int t = *a;  
+    *a = *b;  
+    *b = t;  
+}  
+
+int divideyvenceras (int arr[], int low, int high)  
+{  
+    int pivot = arr[high]; 
+    int i = (low - 1);
+  
+    for (int j = low; j <= high - 1; j++)  
+    {  
+     
+        if (arr[j] < pivot)  
+        {  
+            i++;  
+            cambio(&arr[i], &arr[j]);  
+        }  
+    }  
+    cambio(&arr[i + 1], &arr[high]);  
+    return (i + 1);  
+}  
+  
+
+void quickSort(int arr[], int low, int high)  
+{  
+    if (low < high)  
+    {  
+
+        int pi = divideyvenceras(arr, low, high);  
+  
+        quickSort(arr, low, pi - 1);  
+        quickSort(arr, pi + 1, high);  
+    }  
+}  
+
+void mostrar(int arr[], int size)  
+{  
+    int i;  
+    for (i = 0; i < size; i++)  
+        cout << arr[i] << " ";  
+    cout << endl;  
+}  
+  
+int main()  
+{  
+    int arr[] = {10, 7, 8, 9, 1, 5};  
+    int n = sizeof(arr) / sizeof(arr[0]);  
+    quickSort(arr, 0, n - 1);    
+    mostrar(arr, n);  
+    return 0;  
+}  
